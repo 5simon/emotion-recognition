10adf255d7e3452e3ca96d76a2fb87e9fbe29a25
5simon <alnomersimon@gmail.com>
2022-10-14 15:26:41 +0200

    class Camera, open camera is working, still saving all frames

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import tensorflow as tf
+
 from Recognition.camera import *
 
 print("tensorflow is installed and has the version: ", tf.__version__)
 print("\nopencv is installed and has the version: ", cv.__version__)
 
-#saveVideoAndImageFromCamera()
-faceRecognition()
+# saveVideoAndImageFromCamera()
+# faceRecognition()
+# openCamere()
+
+cameraWindow = Camera()
+cameraWindow.openCamere()
+# cameraWindow.saveVideoAndImageFromCamera()
+cameraWindow.closeCamera()
