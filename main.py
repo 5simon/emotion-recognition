57f8cda859fac82532e3e5c2136588d7b8f3a1d0
5simon <alnomersimon@gmail.com>
2022-10-19 19:24:50 +0200

    function resize images

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from Recognition.face.camera import *
 from Recognition.Emotion.helpFunctions import *
+import tensorflow as tf
 
 print("tensorflow is installed and has the version: ", tf.__version__)
 print("\nopencv is installed and has the version: ", cv.__version__)
 
 
 
-camera_window = Camera()
-camera_window.save_image_from_camera()
+# camera_window = Camera()
+# camera_window.save_image_from_camera()
 
 
 
-# load_dataset()
+load_dataset()
 
