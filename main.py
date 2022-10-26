ddf758593c78e35f53aa830866548be3be4105e4
5simon <alnomersimon@gmail.com>
2022-10-26 15:32:55 +0200

    add args to run programm-- still fo fix

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from Recognition.Emotion.model import Model
 from Recognition.face.camera import Camera
+import tensorflow as tf
+import cv2
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--test", type=str ,help="test the model")
+parser.add_argument("-l", "--train", type=str, help="train model")
+mode = parser.parse_args()
 
 if __name__ == "__main__":
-    # camera_window = Camera(which_camera=0)
-    # camera_window.save_image_from_camera()
 
-    model1 = Model()
-    model1.save_model_info()
+    if mode == "test":
+        camera_window = Camera(which_camera=0)
+        camera_window.save_image_from_camera()
+
+    if mode == "train":
+        model1 = Model()
+        model1.save_model_info()
