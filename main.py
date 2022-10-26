05dc779c91c8ab829639c2e076df5cb4e9611813
5simon <alnomersimon@gmail.com>
2022-10-26 15:06:38 +0200

    add class model

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from Recognition.Emotion.test_model import TestModel
+from Recognition.Emotion.model import Model
 from Recognition.face.camera import Camera
-import cv2
-if __name__ == "__main__":
-    camera_window = Camera(which_camera=0)
-    camera_window.save_image_from_camera()
 
-    # test_1 = TestModel("Recognition/Emotion/model_1/model.json", "Recognition/Emotion/model_1/model.h5")
-    # test_1.emotion_recognition()
+if __name__ == "__main__":
+    # camera_window = Camera(which_camera=0)
+    # camera_window.save_image_from_camera()
+
+    model1 = Model()
+    model1.save_model_info()
