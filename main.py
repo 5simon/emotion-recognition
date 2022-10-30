9d9fea65bcb0a3d29c4b366552c58ac66dc1d044
5simon <alnomersimon@gmail.com>
2022-10-30 16:06:13 +0100

    argpares is working

--- main.py
+++ main.py
@@ -5,16 +5,15 @@ import cv2
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-t", "--test", type=str ,help="test the model")
+parser.add_argument("-t", "--test", type=str, help="test the model")
 parser.add_argument("-l", "--train", type=str, help="train model")
 mode = parser.parse_args()
 
 if __name__ == "__main__":
-
-    if mode == "test":
+    if mode.test:
         camera_window = Camera(which_camera=0)
         camera_window.save_image_from_camera()
 
-    if mode == "train":
+    if mode.train:
         model1 = Model()
         model1.save_model_info()
