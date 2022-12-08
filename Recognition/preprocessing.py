9a1513440c6f2521c4fe2e62eb42ae40f7d9f833
5simon <alnomersimon@gmail.com>
2022-12-08 12:58:39 +0100

    drawing rectangel about eyes and mouth

--- Recognition/preprocessing.py
+++ Recognition/preprocessing.py
@@ -23,6 +23,7 @@ ALL_POINTS = list(range(0, 68))
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--face", type=str, help= "save images with detected face")
 parser.add_argument("-em", "--eyeMouth", type=str, help="save images with just detected mouth and eyes")
+parser.add_argument("-tt", "--test", type=str, help="Test")
 mode = parser.parse_args()
 def draw_shape_lines_range(np_shape, image, range_points, is_closed=False):
     """Draws the shape using lines to connect the different points"""
@@ -30,6 +31,9 @@ def draw_shape_lines_range(np_shape, image, range_points, is_closed=False):
     np_shape_display = np_shape[range_points]
     points = np.array(np_shape_display, dtype=np.int32)
     cv2.polylines(image, [points], is_closed, (255, 255, 0), thickness=1, lineType=cv2.LINE_8)
+    # print(points)
+    # print("-------------------")
+    # cv2.rectangle(image, (ALL_POINTS[23], ALL_POINTS[27]), (ALL_POINTS[17], ALL_POINTS[29]), (0, 255, 0), 1)
 
 
 def draw_shape_points_pos_range(np_shape, image, points):
@@ -165,81 +169,106 @@ def preprocessing_images_detect_face(old_path, new_path):
             # cv2.waitKey(0)
             # cv2.destroyAllWindows()
 
+if mode.test:
+    p = "/home/simon/BA/emotion-recognition/shape_predictor_68_face_landmarks.dat"
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(p)
 
-# image_test = cv2.imread("../me.jpg")
-# image_test_1 = cv2.resize(image_test, (512, 512))
-# gray_test = cv2.cvtColor(image_test_1, cv2.COLOR_BGR2GRAY)
-# rects_test = detector(gray_test, 0)
-#
-#
-# start_punkt, end_punkt = [0, 0], [0, 0]
-#
-# for (i, rect) in enumerate(rects_test):
-#     cv2.rectangle(image_test, (rect.left(), rect.top()), (rect.right(), rect.bottom()), (0, 255, 0), 1)
-#     start_punkt = rect.left(), rect.top()
-#     end_punkt = rect.right(), rect.bottom()
-# cropped_img = np.expand_dims(np.expand_dims(resize_images(image_test_1, 512), -1), 0)
-# black_background = np.zeros(image_test_1.shape[:2], np.uint8)
-# cv2.rectangle(black_background, start_punkt, end_punkt, (255, 255, 255), -1)
-# frame_masked = cv2.bitwise_and(image_test_1, image_test_1, mask=black_background)
-# cv2.imshow("just the face", frame_masked)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+    video_capture = cv2.VideoCapture(2)
+    while True:
 
-# while True:
-#
-#     # Capture frame from the VideoCapture object:
-#     ret, frame = video_capture.read()
-#
-#     # Just for debugging purposes:
-#     # frame = test_face.copy()
-#
-#     # Convert frame to grayscale:
-#     # frame = cv2.resize(frame,(0,0),fx = 0.5 , fy = 0.5)
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#
-#     # Detect faces:
-#     rects = detector(gray, 0)
-#
-#     # For each detected face, find the landmark.
-#     for (i, rect) in enumerate(rects):
-#         # Draw a box around the face:
-#         cv2.rectangle(frame, (rect.left(), rect.top()), (rect.right(), rect.bottom()), (0, 255, 0), 1)
-#
-#         # Get the shape using the predictor:
-#         shape = predictor(gray, rect)
-#
-#         # Convert the shape to numpy array:
-#         shape = shape_to_np(shape)
-#
-#         # Draw all lines connecting the different face parts:
-#         # draw_shape_lines_all(shape, frame)
-#
-#         # Draw jaw line:
-#         draw_shape_lines_range(shape, frame, JAWLINE_POINTS)
-#
-#         # Draw all points and their position:
-#         draw_shape_points_pos(shape, frame)
-#         # You can also use:
-#         # draw_shape_points_pos_range(shape, frame, ALL_POINTS)
-#
-#         # Draw all shape points:
-#         draw_shape_points(shape, frame)
-#
-#         # Draw left eye, right eye and bridge shape points and positions
-#         # draw_shape_points_pos_range(shape, frame, LEFT_EYE_POINTS + RIGHT_EYE_POINTS + NOSE_BRIDGE_POINTS)
-#
-#     # Display the resulting frame
-#     # out.write(frame)
-#     cv2.imshow("Landmarks detection using dlib", frame)
-#
-#     # Press 'q' key to exit
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-#
-# # Release everything:
-# video_capture.release()
-# cv2.destroyAllWindows()
+        # Capture frame from the VideoCapture object:
+        ret, frame = video_capture.read()
+
+        # Just for debugging purposes:
+        # frame = test_face.copy()
+
+        # Convert frame to grayscale:
+        # frame = cv2.resize(frame,(0,0),fx = 0.5 , fy = 0.5)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Detect faces:
+        rects = detector(gray, 0)
+
+        # For each detected face, find the landmark.
+        for (i, rect) in enumerate(rects):
+            # Draw a box around the face:
+            cv2.rectangle(frame, (rect.left(), rect.top()), (rect.right(), rect.bottom()), (0, 255, 0), 1)
+            # Get the shape using the predictor:
+            shape = predictor(gray, rect)
+
+            # eye1 = cv2.rectangle(frame, (shape.part(18).x, shape.part(18).y),(shape.part(37).x, shape.part(37).y),(0, 255, 0), 5)
+            eye1 = cv2.rectangle(frame, (shape.part(17).x, shape.part(17).y-15),(shape.part(28).x, shape.part(28).y+15),(255,0, 0), 5)
+            eye1 = cv2.rectangle(frame, (shape.part(15).x, shape.part(18).y-15),(shape.part(28).x, shape.part(28).y+15),(0,255, 0), 5)
+            eye1 = cv2.rectangle(frame, (shape.part(49).x-20, shape.part(49).y-15),(shape.part(55).x+20, shape.part(55).y+15),(0,0,255), 5)
+
+
+            # eye1 = cv2.circle(frame, (shape.part(37).x, shape.part(37).y), radius=0, color=(0, 255, 0), thickness=3)
+            # eye1 = cv2.circle(frame, (shape.part(38).x, shape.part(38).y), radius=0, color=(0, 255, 0), thickness=3)
+            # eye1 = cv2.circle(frame, (shape.part(39).x, shape.part(39).y), radius=0, color=(0, 255, 0), thickness=3)
+            # eye1 = cv2.circle(frame, (shape.part(40).x, shape.part(40).y), radius=0, color=(0, 255, 0), thickness=3)
+            # eye1 = cv2.circle(frame, (shape.part(41).x, shape.part(41).y), radius=0, color=(0, 255, 0), thickness=3)
+            #
+            # eye1_b = cv2.circle(frame, (shape.part(17).x, shape.part(17).y), radius=0, color=(0, 255, 0), thickness=3)
+            # eye1_b = cv2.circle(frame, (shape.part(18).x, shape.part(18).y), radius=0, color=(0, 255, 0), thickness=3)
+            # eye1_b = cv2.circle(frame, (shape.part(19).x, shape.part(19).y), radius=0, color=(0, 255, 0), thickness=3)
+            # eye1_b = cv2.circle(frame, (shape.part(20).x, shape.part(20).y), radius=0, color=(0, 255, 0), thickness=3)
+            # eye1_b = cv2.circle(frame, (shape.part(21).x, shape.part(21).y), radius=0, color=(0, 255, 0), thickness=3)
+            # eye1_b = cv2.circle(frame, (shape.part(22).x, shape.part(22).y), radius=0, color=(0, 255, 0), thickness=3)
+            #
+            # eye1_b2 = cv2.circle(frame, (shape.part(23).x, shape.part(23).y), radius=0, color=(0, 255, 0), thickness=3)
+            # eye1_b2 = cv2.circle(frame, (shape.part(24).x, shape.part(24).y), radius=0, color=(0, 255, 0), thickness=3)
+            # eye1_b2 = cv2.circle(frame, (shape.part(25).x, shape.part(25).y), radius=0, color=(0, 255, 0), thickness=3)
+            # eye1_b2 = cv2.circle(frame, (shape.part(26).x, shape.part(26).y), radius=0, color=(0, 255, 0), thickness=3)
+            # eye1_b2 = cv2.circle(frame, (shape.part(27).x, shape.part(27).y), radius=0, color=(0, 255, 0), thickness=3)
+            # # eye1_b2 = cv2.circle(frame, (shape.part(28).x, shape.part(28).y), radius=0, color=(0, 255, 0), thickness=3)
+            #
+            # eye1 = cv2.circle(frame, (shape.part(42).x , shape.part(42).y),radius=0, color=(0, 255, 0), thickness=3)
+            # eye1 = cv2.circle(frame, (shape.part(43).x, shape.part(43).y), radius=0, color=(0, 255, 0), thickness=3)
+            # eye1 = cv2.circle(frame, (shape.part(44).x , shape.part(44).y), radius=0, color=(0, 255, 0), thickness=3)
+            # eye1 = cv2.circle(frame, (shape.part(45).x , shape.part(45).y),radius=0, color=(0, 255, 0), thickness=3)
+            # eye1 = cv2.circle(frame, (shape.part(46).x , shape.part(46).y),radius=0, color=(0, 255, 0), thickness=3)
+            # eye1 = cv2.circle(frame, (shape.part(47).x, shape.part(47).y), radius=0, color=(0, 255, 0), thickness=3)
+
+            # Convert the shape to numpy array:
+
+            shape = shape_to_np(shape)
+
+            # Draw all lines connecting the different face parts:
+            # draw_shape_lines_all(shape, frame)
+
+            # Draw jaw line:
+            # draw_shape_lines_range(shape, frame, RIGHT_EYEBROW_POINTS)
+            # draw_shape_lines_range(shape, frame, LEFT_EYEBROW_POINTS)
+            # draw_shape_lines_range(shape, frame, LEFT_EYE_POINTS)
+            # draw_shape_lines_range(shape, frame, RIGHT_EYE_POINTS)
+            # draw_shape_lines_range(shape, frame, MOUTH_INNER_POINTS)
+            # draw_shape_lines_range(shape, frame, MOUTH_OUTLINE_POINTS)
+
+
+
+            # Draw all points and their position:
+            # draw_shape_points_pos(shape, frame)
+            # You can also use:
+            # draw_shape_points_pos_range(shape, frame, ALL_POINTS)
+
+            # Draw all shape points:
+            # draw_shape_points(shape, frame)
+
+            # Draw left eye, right eye and bridge shape points and positions
+            # draw_shape_points_pos_range(shape, frame, LEFT_EYE_POINTS + RIGHT_EYE_POINTS + NOSE_BRIDGE_POINTS)
+
+        # Display the resulting frame
+        cv2.imshow("Landmarks detection using dlib", frame)
+
+        # Press 'q' key to exit
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release everything:
+    video_capture.release()
+    cv2.destroyAllWindows()
+
 if mode.face:
     ''' Train data '''
     print("it starts for train data")
