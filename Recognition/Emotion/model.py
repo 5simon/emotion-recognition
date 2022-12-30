f4044f1904e34fba9ee0898a20062266ab3a8f60
5simon <alnomersimon@gmail.com>
2022-12-30 09:55:46 +0100

    new model eye mouth

--- Recognition/Emotion/model.py
+++ Recognition/Emotion/model.py
@@ -18,20 +18,20 @@ from scipy.ndimage import label
 
 class Model:
 
-    index_train_images = 28709
-    index_validation_images = 7178
+#    index_train_images = 28709
+#    index_validation_images = 7178
 
     # this is for the last training, I deleted the images which coulde't be processed
-    #index_train_images = 20349
-    #index_validation_images = 5074
+    index_train_images = 20349
+    index_validation_images = 5074
     epoches = 50
     batch_size = 64
     image_size = 48
-    file_name_train = "/home/simon/BA/data_face/train"
-    file_name_test = "/home/simon/BA/data_face/test"
-    def __init__(self, index_train_images=28709, index_validatiyon_images=7178, epoches=50, batch_size=64, image_size=48):
+    file_name_train = "/home/simon/BA/data/train"
+    file_name_test = "/home/simon/BA/data/test"
+    def __init__(self, index_train_images=20349, index_validatiyon_images=5074, epoches=50, batch_size=64, image_size=48):
         self.index_train_images = index_train_images
-        self.index_validation_images = index_validation_images
+        self.index_validation_images = index_validatiyon_images
         self.epoches = epoches
         self.batch_size = batch_size
         self.image_size = image_size
